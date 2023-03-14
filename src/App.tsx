cd1afc643fb776247fdfc18356435a76776b3117
Nikita Salms <salms.nikita@yandex.ru>
2023-03-14 20:39:26 +0300

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Header from "./components/Header/Header";
 import ContentMain from "./components/ContentMain/ContentMain";
+import background from "./assets/img/background.png"
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" style={{overflow: "hidden", position: "relative"}}>
+        <picture className="background">
+            <img src={background} alt="" decoding="async" />
+        </picture>
         <div className="container">
             <Header />
             <ContentMain />
+            <Footer />
         </div>
     </div>
   )
