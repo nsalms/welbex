ef7d667ef9e6954197e8ef00b594010ad7137d66
Nikita Salms <salms.nikita@yandex.ru>
2023-03-14 20:39:48 +0300

    add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import s from "./Button.module.scss"
+
+interface ButtonProps {
+    text: String
+}
+
+function Button({text}: ButtonProps) {
+    return (
+        <button className={s.btn}>{text}</button>
+    )
+}
+
+export default Button
